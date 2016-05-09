a62486fd994676512d1f8c8abf6cf1b7c30e32c0
Mountain97 <emailcc@mac.com>
2016-05-08 21:08:41 -0600

    First commit

---
+++ The Sixth Station/example-audioInput/src/ofApp.cpp
@@ -0,0 +1,127 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    ofSetBackgroundAuto(false);
+
+	ofSetVerticalSync(true);
+	ofBackground(0,0,0);
+	ofSetFrameRate(60);
+
+    int ticksPerBuffer = 8;
+    bufferSize = BUFFER_SIZE; //pdBlock-64
+    inChan  = 2;
+	outChan = 0;
+	sampleRate = SR;
+
+    int midiMin = 45;
+    int midiMax = 90;
+    
+    filterBank.setup(bufferSize, midiMin, midiMax, inChan, BANDWITH, sampleRate, 1.0);
+    filterBank.setColor(ofColor::orange);
+
+	soundStream.setup(this, outChan, inChan, sampleRate, bufferSize, ticksPerBuffer);	}
+
+
+//--------------------------------------------------------------
+
+void ofApp::update(){
+    
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+	
+	
+    float chSz = bufferSize/3;
+		{
+        
+            filterBank.draw(1024,768);
+        
+	}
+	
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::audioIn(float * input, int bufferSize, int nChannels){
+    
+    //Analyze Input Buffer with ofxFilterbank
+    filterBank.analyze(input);
+    
+}
+
+
+void ofApp::audioOut(float * output, int bufferSize, int nChannels){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed  (int key){
+    switch (key){
+        case 'q':
+            filterBank.showAll = !filterBank.showAll;
+			break;
+        case 'c':
+            ofBackground(0,0,0,10);
+        default:
+            break;
+    }
+}
+
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+    soundStream.stop();
+    soundStream.close();
+    filterBank.exit();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
+
---
+++ The Sixth Station/src/ofxFilterbank.cpp
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2015 Leo Zimmerman <leozimmerman@gmail.com>
+ */
+
+#include "ofxFilterbank.h"
+
+
+//--------------------------------------------------------------
+void ofxFilterbank::setup(int iBufferSize, int iMidiMin, int iMidiMax, int iChans, float iBw, int iSR, float iGain){
+
+    bufferSize = iBufferSize;
+    midiMaxVar = midiMax = iMidiMax;
+    midiMinVar = midiMin = iMidiMin;
+    channels = iChans;
+    bw = iBw;
+    sampleRate = iSR;
+    gain = iGain;
+
+    pitchDev = 0.0;
+    maskAmnt = 0.0;
+    smoothAmnt = 0.7;
+    estimateMax = 0.2;
+
+    treshold = 0.00;
+    showAll = true;
+
+    left.assign(bufferSize, 0.0);
+	right.assign(bufferSize, 0.0);
+
+
+    energies.assign(midiMax, 0.0);
+    smth_energies.assign(midiMax, 0.0);
+    log_smth_energies.assign(midiMax, 0.0);
+
+   fdata=(RESONDATA***)malloc(sizeof(RESONDATA**)*channels);
+   for (int i=0; i<channels; i++) {
+         fdata[i]=(RESONDATA**)malloc(sizeof(RESONDATA*)*midiMax);
+         for (int n=midiMin; n<midiMax; n++){
+            double exp = (double)(n-69+pitchDev)/12;
+            double frec = pow (2., exp)*440;
+            fdata[i][n]=(RESONDATA*)malloc(sizeof(RESONDATA));
+            set_reson(frec, bw,fdata[i][n], sampleRate, gain);
+            set_reson(frec, bw,fdata[i][n], sampleRate, gain);
+        }
+    }
+    color.set(255);
+}
+//--------------------------------------------------------------
+void ofxFilterbank::setPitchDev(){
+    for (int i=0; i<channels; i++) {
+        // fdata[i]=(RESONDATA**)malloc(sizeof(RESONDATA*)*midiMax);
+         for (int n=midiMin; n<midiMax; n++){
+            double exp = (double)(n-69+pitchDev)/12;
+            double frec = pow (2., exp)*440;
+          //  fdata[i][n]=(RESONDATA*)malloc(sizeof(RESONDATA));
+            set_reson(frec, bw,fdata[i][n], sampleRate, gain);
+            set_reson(frec, bw,fdata[i][n], sampleRate, gain);
+        }
+    }
+}
+//--------------------------------------------------------------
+void ofxFilterbank::analyze(float * iBuffer){
+    //Reset energies
+
+    for (int n=midiMinVar; n<midiMaxVar; n++){
+            energies[n] = 0.0;
+    }
+     //Process
+    for (int i = 0; i < left.size(); i++){
+        left[i] = iBuffer[i*channels    ] ;
+        right[i]= iBuffer[i*channels + 1];
+
+        for (int n=midiMinVar; n<midiMaxVar;n++){
+            float lOutsample = 0.0;
+            float rOutsample = 0.0;
+
+            lOutsample = reson(left[i], fdata[0][n]);
+            energies[n]  += lOutsample*lOutsample;
+            if (channels>1){
+                rOutsample = reson(right[i], fdata[1][n]);
+                energies[n] += rOutsample*rOutsample;
+            }
+        }
+	 }
+
+	 //Smoothing...
+
+    for (int n=midiMaxVar-1; n>=midiMinVar;n--){
+        energies[n] /= (left.size()*channels);
+        energies[n] = sqrt(energies[n]);
+        smth_energies[n] *= smoothAmnt;
+        smth_energies[n] += (1-smoothAmnt) * energies[n];
+        ///mask
+        if(smth_energies[n]>treshold  && n>midiMinVar+1){
+            if (smth_energies[n]>smth_energies[n-1])
+                smth_energies[n-1] -= smth_energies[n]*maskAmnt;
+            else
+                smth_energies[n] -= smth_energies[n-1]*maskAmnt;
+    
+            if (n>11 /*&& smth_energies[n]>smth_energies[n-12]*0.35*/ )///*
+                smth_energies[n] -= smth_energies[n-12]*maskAmnt;
+        }
+    }
+
+}
+//--------------------------------------------------------------
+void ofxFilterbank::exit(){
+    
+    for (int i=0; i<channels; i++) {
+        for (int n=midiMin; n<midiMax; n++){
+            if (fdata[i][n]!=NULL ) free (fdata[i][n]);
+        }
+        free (fdata[i]);
+    }
+    free (fdata);
+    
+}
+//--------------------------------------------------------------
+void ofxFilterbank::draw(int w, int h){
+
+    ofSetColor(255);
+    //ofDrawBitmapString("FB Analysis", 4, 18);
+    //ofRect(0, 0, w, h);
+    float step = (float) w / (midiMaxVar - midiMinVar);
+    ofSetLineWidth(100);
+    
+    for(int n=midiMinVar; n<midiMaxVar; n++){
+        log_smth_energies[n] = LIN2dB (smth_energies[n]);
+      
+       float ener = ofMap (log_smth_energies[n], -45.0, LIN2dB(estimateMax), 0.0, h, true);
+        if(smth_energies[n] > treshold){
+            //ofSetColor(color);
+            ofDrawBitmapString(midiToNote(n), step*(n-midiMinVar), h-ener);
+            ofDrawCircle(step*(n-midiMinVar), ((h-ener) + 15),15);
+        
+            
+        }
+        
+    }
+    
+
+    /*ofSetColor(80);
+    float logTresh = LIN2dB(treshold);
+    float tres = ofMap(logTresh, -45.0, LIN2dB(estimateMax), 0.0, h);
+    ofSetLineWidth(1);
+    ofLine(0,h-tres, w, h-tres);
+    ofDrawBitmapString("Treshold" , w-80, h-tres);*/
+
+
+
+}
+//--------------------------------------------------------------
+string ofxFilterbank::midiToNote(int midi){
+    string noteName;
+    int mod = midi%12;
+    switch (mod){
+        case 0: noteName = " "; //C
+            ofSetColor(77,163,137,8);//most green
+            break;
+        case 1: noteName = " "; //C#
+            ofSetColor(75,149,143,8);
+            break;
+        case 2: noteName = " "; //D
+            ofSetColor(72,135,149,8);
+            break;
+        case 3: noteName = " "; //D#
+            ofSetColor(70,121,155,8);
+            break;
+        case 4: noteName = " ";
+            ofSetColor(68,107,161,8);
+            break;
+        case 5: noteName = " ";
+            ofSetColor(65,93,167,8);
+            break;
+        case 6: noteName = " ";
+            ofSetColor(60,77,176,8);//most blue
+            break;
+        case 7: noteName = " ";
+            ofSetColor(64,96,174,8);
+            break;
+        case 8: noteName = " ";
+            ofSetColor(68,115,171,8);
+            break;
+        case 9: noteName = " ";
+            ofSetColor(72,134,169,8);
+            break;
+        case 10: noteName = " ";
+            ofSetColor(76,143,166,8);
+            break;
+        case 11: noteName = " ";
+            ofSetColor(77,162,163,8);
+            break;
+        default:
+            break;
+            
+    }
+    return (noteName);
+
+}
